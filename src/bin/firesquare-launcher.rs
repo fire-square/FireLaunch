d0a076f1faec47789321df8bbf37d42104220a4f
cofob <cofob@riseup.net>
2022-12-26 20:44:45 +0700

    Prevents console window from appearing on Windows

--- src/bin/firesquare-launcher.rs
+++ src/bin/firesquare-launcher.rs
@@ -1,3 +1,6 @@
+// Prevents console window from appearing on Windows
+#![windows_subsystem = "windows"]
+
 use firesquare_launcher::gui::AppModel;
 use firesquare_launcher::utils::init_logging;
 use firesquare_launcher::{NAME, VERSION};
