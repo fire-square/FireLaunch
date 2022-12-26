16640ef5699ea1c97652dbfeefabaa71795e905c
cofob <cofob@riseup.net>
2022-12-26 22:26:02 +0700

    Add set icon name

--- src/gui/app.rs
+++ src/gui/app.rs
@@ -50,6 +50,7 @@ impl SimpleComponent for AppModel {
 		#[root]
 		gtk::ApplicationWindow {
 			set_title: Some("FireLaunch"),
+			set_icon_name: Some("firesquare-launcher"),
 			gtk::Box {
 				set_orientation: gtk::Orientation::Vertical,
 				set_spacing: 5,
